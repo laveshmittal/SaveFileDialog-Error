eb2070f7b9882ba4a7499ee0b0311e1b81e6b706
Lavesh Mittal <mittall@lenze.com>
2022-01-25 13:08:42 +0530

    Add project files.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Random selection (*.rpc)|*.rpc";
+            saveFileDialog.ShowHelp = true;
+            saveFileDialog.HelpRequest += (object sender, EventArgs eventArgs) =>{};
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                WithHelpFileName.Text = saveFileDialog.FileName;
+            }
+            else
+            {
+                WithHelpFileName.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Random selection (*.rpc)|*.rpc";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                WithoutHelpFileName.Text = saveFileDialog.FileName;
+            }
+            else
+            {
+                WithoutHelpFileName.Text = "";
+            }
+        }
+    }
+}
